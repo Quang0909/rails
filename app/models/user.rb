1560ec5cd552fee08c6a20b0db7133daf94148ca
quangnv <nvquang9996@gmail.com>
2019-04-10 15:56:01 +0700

    chap6

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 50 }
+
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  before_save :downcase_email
+  has_secure_password
+
+  def downcase_email
+end
